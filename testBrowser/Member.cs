ebe681f12d0c0a3dc93978e870dcfa288593701b
reirin <reirin_nyanko_88@yahoo.co.jp>
2014-11-27 19:17:25 +0900

    作成
    これから頑張るぞい

---
+++ testBrowser/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testBrowser
+{
+    class Member
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
